9403682c74a89a2929f64b6eb8176554622ffc48
Mohamad Nurcahyo Arifianto <53028055+nurcahyo3074@users.noreply.github.com>
2025-06-26 09:08:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,105 @@
+<!created by Mohamad Nurcahyo>
+<!version 2.4 Sep 2024>
+
+<html>
+<head>
+  <title>OS Compliance Linux Site</title>
+  <meta name="GENERATOR" content="MY-CBJ-Linuxcompliance">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css" rel="stylesheet">
+  <style>
+i,span {
+  font-size: 50px;
+}
+a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+a:active {
+  text-decoration: none;
+}
+  </style>
+</head>
+<body>
+<div class="container-fluid">
+<h1 style="background-color:gold;color:red;"><i>OS LINUX COMPLIANCE</i></h1>
+<h2>Please choose the link:</h2>
+<div class="row">
+<div class="col-md-4">
+        <a href="http://mykullstc000624.apis.dhl.com/tms/tms.php"> <!-- Ini adalah link -->
+                <div class="card text-white bg-primary mb-3">
+                  <div class="card-header">
+                        <i class="ti-home"></i> <!-- Ini adalah icon -->
+                        TMS Last Server Reboot
+                  </div>
+                  <div class="card-body bg-light text-dark">
+                        <p class="card-text">Show information about all TMS last reboot time</p>
+                  </div>
+                  <div class="card-footer">
+                        <!span class="float-right badge badge-light"><!/span>
+                  </div>
+                </div>
+        </a>
+</div>
+
+<div class="col-md-4">
+        <a href="https://mykulls4556.apis.dhl.com/LinuxOS/inventory/index.html"> <!-- Ini adalah link -->
+                <div class="card text-white bg-dark mb-3">
+                  <div class="card-header">
+                        <i class="ti-shine"></i> <!-- Ini adalah icon -->
+                        Software Inventory Page
+                  </div>
+                  <div class="card-body bg-light text-dark">
+                        <p class="card-text">Software Information for Linux,HPUX & AIX</p>
+                  </div>
+                  <div class="card-footer">
+                        <!span class="float-right badge badge-success"><!/span>
+                  </div>
+                </div>
+        </a>
+</div>
+<div class="col-md-4">
+        <a href="https://mykulls4556.apis.dhl.com/ppp/home.html"> <!-- Ini adalah link -->
+                <div class="card text-white bg-success mb-3">
+                  <div class="card-header">
+                        <i class="ti-mouse-alt"></i> <!-- Ini adalah icon -->
+                        Patching Profile Portal (PPP)
+                  </div>
+                  <div class="card-body bg-light text-dark">
+                        <p class="card-text">Provides the Patching Profile For Kernel and Veritas</p>
+                  </div>
+                  <div class="card-footer">
+                        <!span class="float-right badge badge-danger"><!/span>
+                  </div>
+                </div>
+        </a>
+</div>
+<div class="col-md-4">
+        <a href="http://mykullstc000624.apis.dhl.com/qualys/qualys_getfolder.php"> <!-- Ini adalah link -->
+                <div class="card text-white bg-danger mb-3">
+                  <div class="card-header">
+                        <i class="ti-harddrive"></i> <!-- Ini adalah icon -->
+                        Qualy Get Logs and Folders
+                  </div>
+                  <div class="card-body bg-light text-dark">
+                        <p class="card-text">Qualys get log files and folders</p>
+                  </div>
+                  <div class="card-footer">
+                        <!span class="float-right badge badge-danger"><!/span>
+                  </div>
+                </div>
+        </a>
+</div>
+
+</div>
+</div>
+</body>
+</html>
